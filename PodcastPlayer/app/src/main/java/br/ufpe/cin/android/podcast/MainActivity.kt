b469651d9779e4f932cd24b79a55857bf8dc5424
Andre De Carli <acms@cin.ufpe.br>
2019-09-17 12:39:37 -0300

    Passo 2

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -1,12 +1,25 @@
 package br.ufpe.cin.android.podcast
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Carrega de maneira assíncrona o arquivo XML
+        doAsync {
+            var xmlContent = URL("https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml").readText()
+            var itemFeedList = Parser.parse(xmlContent)
+        }
     }
+
+
+
 }
