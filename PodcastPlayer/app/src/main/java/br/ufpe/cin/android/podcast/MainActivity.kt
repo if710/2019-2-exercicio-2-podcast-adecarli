3d72eaad59378aca96e8350e4b52755bfe809272
Andre De Carli <acms@cin.ufpe.br>
2019-09-18 15:23:56 -0300

    Passo 6

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import java.lang.Exception
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
@@ -18,17 +19,26 @@ class MainActivity : AppCompatActivity() {
         val db = ItemsDB.getDatabase(this)
         // Carrega de maneira assíncrona o arquivo XML
         doAsync {
-            val xmlContent = URL("http://audio.globoradio.globo.com/podcast/feed/507/2-pontos").readText()
-            val itemFeedList = Parser.parse(xmlContent)
-            uiThread {
-                listRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-                listRecyclerView.adapter = ItemFeedAdapter(itemFeedList, this@MainActivity)
-                listRecyclerView.addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL))
-            }
-            // Armazena os episodios carregados no banco de dados
-            for (item in itemFeedList) {
-                db.itemsDAO().inserirItem(item)
-                println(item)
+            var itemFeedList = listOf<ItemFeed>()
+            // Se o carregamento pela internet falhar, use as informações armazenadas no banco de dados
+            try {
+                val xmlContent = URL("http://audio.globoradio.globo.com/podcast/feed/507/2-pontos").readText()
+                itemFeedList = Parser.parse(xmlContent)
+                // Armazena os episodios carregados no banco de dados
+                for (item in itemFeedList) {
+                    db.itemsDAO().inserirItem(item)
+                }
+
+            } catch (e : Exception) {
+                itemFeedList = db.itemsDAO().todosItens()
+            } finally {
+
+                uiThread {
+                    listRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                    listRecyclerView.adapter = ItemFeedAdapter(itemFeedList, this@MainActivity)
+                    listRecyclerView.addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL))
+                }
+
             }
         }
     }
